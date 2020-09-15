ea4e1da69f45640b9432923fd37832f74b42aab4
Ashutosh Kumar <48728908+ashutro@users.noreply.github.com>
2020-09-15 15:49:29 +0530

    Creating Stack

---
+++ CreateStack.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct stack
+{
+    int size;
+    int top;
+    int *S;
+};
+void createStack(struct stack *st, int size)
+{
+    //Creating stack in heap memory by using the malloc funtion:
+    st->top = -1;
+    st->S = (int *)malloc(st->size * sizeof(int));
+}
+void push(struct stack *st, int data) // here we take 'st' as "Call by reference" :
+{   
+    //checking the condition for stack overflow: by comparing the top isEqual to the stackSize -1:
+    if(st->top == st->size-1)
+    {
+        printf("stack OverFlow:");
+        return;
+    }
+    else
+    {   // with (++ prifix ) first we increse the top then we take the data in the stack:
+        st->top++;
+        st->S[st->top] = data;
+    }
+}
+// iterative display funtion:
+void display(struct stack st) // here we take 'st' as "Call by value" :
+{
+    int i;
+    for(i = st.top; i >= 0; --i)
+    {
+        printf("%d ",st.S[i]);
+    }
+}
+void Rdisplay(struct stack st)  // here we take 'st' as "Call by value" :
+{
+    if(st.top == -1)
+        return;
+    else
+    {
+        printf("%d ",st.S);
+        Rdisplay(st);
+    }
+}
+int main()
+{
+    int i, data;
+    struct stack st;
+    printf("Enter the size of stack: ");
+    scanf("%d",&st.size);
+    createStack(&st,st.size);
+    for(i = 1 ; i <= st.size; i++)
+    {
+        printf("Enter the data %d: ",i);
+        scanf("%d",&data);
+        push(&st,data);
+    }
+    display(st);
+    return 0;
+}
